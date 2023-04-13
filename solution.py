9f1a47d7861a44ecf3b87b35848d8fc72dd99267
cheba1 <113043236+cheba1@users.noreply.github.com>
2023-04-13 23:49:12 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import anderson_ksamp
+from scipy.stats import ks_2samp
 
 chat_id = 725861714 # Ваш chat ID, не меняйте название переменной
 
@@ -8,5 +8,6 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    
-    return anderson_ksamp([x, y]).pvalue < 0.06 # Ваш ответ, True или False
+    alpha = 0.06
+    stat, pval = ks_2samp(x, y)
+    return pval < alpha
